c42e7d6fea3d46093481de7a40a8697bfed3b4f3
ogom <ogom@outlook.com>
2020-08-02 07:46:21 +0900

    Fix config

--- manage.py
+++ manage.py
@@ -8,10 +8,29 @@ def main():
         print("No argument!")
         sys.exit()
 
-    print("Argument: {}".format(sys.argv[1]))
-    print("Git Branch: {}".format(os.environ.get('CIRCLE_BRANCH')))
+    execute_command = sys.argv[1]
+    git_branch = os.environ.get('CIRCLE_BRANCH')
 
-    # subprocess.run(['ls'])
+    print("Execute Command: {}".format(execute_command))
+    print("Git Branch: {}".format(git_branch))
+
+    if execute_command == 'deploy':
+        if git_branch == 'master':
+            subprocess.run([
+                'sam', 'deploy',
+                '--stack-name', 'master-my-sam-pips',
+                '--s3-bucket', 'aws-sam-cli-managed-default-samclisourcebucket-128f92u2xqf55',
+                '--s3-prefix', 'my-sam-pips',
+                '--region', 'ap-northeast-1',
+            ])
+        elif git_branch == 'integrate':
+            subprocess.run([
+                'sam', 'deploy',
+                '--stack-name', 'integrate-my-sam-pips',
+                '--s3-bucket', 'aws-sam-cli-managed-default-samclisourcebucket-1078k6hr9hsla',
+                '--s3-prefix', 'my-sam-pips',
+                '--region', 'us-west-2',
+            ])
 
 
 if __name__ == '__main__':
